8b02e8162278a078237f4eff5b355e24af6986e6
xxxjonasLP <jono_21@web.de>
2025-01-28 08:32:22 +0100

    User class, Product class anfang

---
+++ SnackStop/products.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackStop
+{
+    internal class products
+    {
+        public products() { }
+
+        public products(int product_id, string name, string description, string price, int stock_quanity, int total_purchased)
+        {
+            this.product_id = product_id;
+            this.name = name;
+            this.description = description;
+            this.price = price;
+            this.stock_quanity = stock_quanity;
+            this.total_purchased = total_purchased;
+        }
+
+        private int product_id;
+        private string name;
+        private string description;
+        private string price;
+        private int stock_quanity;
+        private int total_purchased; //glaube int ist bissn small
+
+    }
+}
---
+++ SnackStop/users.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackStop
+{
+    internal class users
+    {
+        public users() { }
+
+        public users(int user_id, string first_name, string last_name, string straße, string city, int postal_code, string address, string username, string password, string email, string created_at)
+        {
+            this.user_id = user_id;
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.straße = straße;
+            this.city = city;
+            this.postal_code = postal_code;
+            this.address = address;
+            this.username = username;
+            this.password = password;
+            this.email = email;
+            this.created_at = created_at;
+        }
+
+        private int user_id;
+        private string first_name;
+        private string last_name;
+        private string straße;
+        private string city;
+        private int postal_code;
+        private string address;
+        private string username;
+        private string password;
+        private string email;
+        private string created_at;
+
+    }
+}
