44d72f8a877f78981dda8eec08aaef7c4c07c4cb
xxxjonasLP <jono_21@web.de>
2025-01-28 08:46:11 +0100

    supplier class anfang

--- SnackStop/Supplier.cs
+++ SnackStop/Supplier.cs
@@ -6,23 +6,20 @@ using System.Threading.Tasks;
 
 namespace SnackStop
 {
-    internal class users
+    internal class Supplier
     {
-        public users() { }
+        public Supplier() { }
 
-        public users(int user_id, string first_name, string last_name, string straße, string city, int postal_code, string address, string username, string password, string email, string created_at)
+        public Supplier(int user_id, string first_name, string last_name, string city, int postal_code, string address, string email, int tel)
         {
             this.user_id = user_id;
             this.first_name = first_name;
             this.last_name = last_name;
-            this.straße = straße;
             this.city = city;
             this.postal_code = postal_code;
             this.address = address;
-            this.username = username;
-            this.password = password;
             this.email = email;
-            this.created_at = created_at;
+            this.tel = tel;
         }
 
         private int user_id;
@@ -31,10 +28,8 @@ namespace SnackStop
         private string city;
         private int postal_code;
         private string address;
-        private string username;
-        private string password;
         private string email;
-        private string created_at;
+        private int tel;
 
     }
 }
